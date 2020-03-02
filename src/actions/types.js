e2e0f03287efdaf0bfe339152e5f8df09827187e
Ian Wolbert <ian.wolbert@avian.com>
2020-03-02 08:09:10 -0500

    Add Redux

---
+++ src/actions/types.js
@@ -0,0 +1,14 @@
+export const GET_LOGS = 'GET_LOGS';
+export const ADD_LOG = 'ADD_LOG';
+export const DELETE_LOG = 'GET_LOGS';
+export const SET_CURRENT = 'GET_LOGS';
+export const CLEAR_CURRENT = 'GET_LOGS';
+export const UPDATE_LOG = 'GET_LOGS';
+export const CLEAR_LOGS = 'GET_LOGS';
+export const SET_LOADING = 'GET_LOGS';
+export const LOGS_ERROR = 'GET_LOGS';
+export const SEARCH_LOGS = 'GET_LOGS';
+export const GET_TECHS = 'GET_LOGS';
+export const ADD_TECH = 'GET_LOGS';
+export const DELETE_TECH = 'GET_LOGS';
+export const TECHS_ERROR = 'GET_LOGS';
